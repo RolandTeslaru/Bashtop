44d0a2e72ee07d813ff949fd645927ff0c2973c4
Roland Teslaru <roland.teslaru@protonmail.com>
2025-12-16 09:40:03 +0200

    added maybe unsued

--- include/monitor/os/AbstractCpuReader.hpp
+++ include/monitor/os/AbstractCpuReader.hpp
@@ -37,7 +37,8 @@ namespace monitor::os {
                 monitor::types::cpu::RawSample& out
             ) = 0;      
 
-            static uint64_t toNanoseconds(const auto &duration){
+            // Cppcheck is dumb
+            [[maybe_unused]] static uint64_t toNanoseconds(const auto &duration){
                 return static_cast<uint64_t>(
                     std::chrono::duration_cast<std::chrono::nanoseconds>(duration).count()
                 );
